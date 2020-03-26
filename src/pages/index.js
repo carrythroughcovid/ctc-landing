fd04b01b3642744f64f1ce034bd533189d23da60
Matt Perrott <matt.perrott@rea-group.com>
2020-03-26 23:06:45 +1100

    add gradient lines to hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,9 +9,25 @@ import Action from "../components/Action"
 const IndexPage = () => (
   <Layout>
     <SEO title="Carry through COVID" />
-    <Hero />
-    <Progress />
     <Action />
+    <svg width="100%" viewBox="0 0 10 1">
+      <polygon
+        stroke="none"
+        stroke-width="0"
+        fill="#7D4CDB"
+        points="0,0.6 0,1 10,1 10,0"
+      />
+    </svg>
+    <Hero />
+    <svg width="100%" viewBox="0 0 10 1">
+      <polygon
+        stroke="none"
+        stroke-width="0"
+        fill="#7D4CDB"
+        points="0,0 0,1 10,0.6 10,0"
+      />
+    </svg>
+    <Progress />
   </Layout>
 )
 
