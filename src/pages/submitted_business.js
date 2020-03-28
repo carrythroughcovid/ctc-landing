88e3ec4b6dd2012e0cdfa39ccae98c0eb85fde23
Matt Perrott <matt.perrott@rea-group.com>
2020-03-28 13:03:37 +1100

    add social links to confirmation page

--- src/pages/submitted_business.js
+++ src/pages/submitted_business.js
@@ -1,11 +1,41 @@
 import React from "react"
 import { navigate } from "gatsby"
 import { Button } from "grommet"
+import styled from "styled-components"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import Container from "../components/Layout/Container"
 import { MainTitle } from "../components/Text"
+import Facebook from "../images/facebook.svg"
+import Instagram from "../images/instagram.svg"
+
+const SocialContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const SocialLink = styled.a`
+  padding-left: 2rem;
+
+  &:visited {
+    color: ${({ theme }) => theme.purple};
+  }
+`
+
+const FacebookLogo = styled(Facebook)`
+  width: 50px;
+  height: 50px;
+`
+
+const InstagramLogo = styled(Instagram)`
+  width: 50px;
+  height: 50px;
+`
+
+const ButtonContainer = styled.div`
+  padding-top: 2rem;
+`
 
 const SubmittedPage = ({ location }) => (
   <Layout>
@@ -13,9 +43,21 @@ const SubmittedPage = ({ location }) => (
     <Container>
       {console.log("submissionType", location.state && location.state.submissionType)}
       <MainTitle>Thanks for submitting!</MainTitle>
-      <p>We will be in touch very shortly. Until then feel free to join our Facebook group or follow us on Instagram!</p>
+      <p>We will be in touch very shortly.</p>
+      <p>Have you seen the Facebook community? Join up to get a headstart on advertising there!</p>
+      <SocialContainer>
+        <FacebookLogo />
+        <SocialLink target="_blank" href="https://facebook.com">facebook.com</SocialLink>
+      </SocialContainer>
+      <p>We are also on Instagram. Message us if you'd like to be featured :)</p>
+      <SocialContainer>
+        <InstagramLogo />
+        <SocialLink target="_blank" href="https://instagram.com">instagram.com</SocialLink>
+      </SocialContainer>
+      <ButtonContainer>
+        <Button onClick={() => navigate("/")} label="Back" />
+      </ButtonContainer>
     </Container>
-    <Button onClick={() => navigate("/")} label="Back" />
   </Layout>
 )
 
